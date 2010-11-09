d4dfe8076fc20907e9d2d6df0af9efb0d7d5a11d
hotrannam <hotrannam@yahoo.com>
2010-11-09 20:58:03 +0700

    remove entity framework and prepare for mongodb

---
+++ MvcMusicStore/Utility/IMongoEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MongoDB.Driver;
+
+namespace MvcMusicStore.Utility
+{
+    public interface IMongoEntity 
+    { 
+        Document InternalDocument { get; set; } 
+    }
+}
---
+++ MvcMusicStore/Utility/Movie.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MongoDB.Driver;
+
+namespace MvcMusicStore.Utility
+{
+    public class Movie : IMongoEntity
+    {
+        public string Name
+        {
+            get { return InternalDocument["Name"].ToString(); }
+            set { InternalDocument["Name"] = value; }
+        }
+
+        public int ProductionYear
+        {
+            get { return (int)InternalDocument["Year"]; }
+            set { InternalDocument["Year"] = value; }
+        }
+
+        public Dictionary<int, string> Actors
+        {
+            get { return JsonConvert.DeserializeObject<Dictionary<int, string>>(InternalDocument["Actors"].ToString()); }
+            set { InternalDocument["Actors"] = value; }
+        }
+
+        public Document InternalDocument { get; set; }
+    }
+}
